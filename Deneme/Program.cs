f077d4df7c1778fbe28830455e37239f877e6fbf
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-01 14:11:24 +0300

    first commit

---
+++ Deneme/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDongusuOdevDeneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sistemUretSayi = 0;
+            int tahminAdet = 1;
+            Random rmd= new Random();
+            sistemUretSayi = rmd.Next(1, 10);
+
+            while (true)
+            {
+                Console.Write(tahminAdet +". Deneme Lütfen tahmin ettiğiniz sayıyı giriniz :");
+                string girilenDeger = Console.ReadLine();
+                int kullaniciDeger = Convert.ToInt32(girilenDeger);
+                if (kullaniciDeger==sistemUretSayi)
+                {
+                    tahminAdet++;
+                    Console.WriteLine(tahminAdet +". Deneme Tebrikler doğru tahmin ettinizz!!");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine(tahminAdet+". Deneme Tahmin edemediniz yeni bir sayı giriniz!");
+                    tahminAdet++;
+                }
+
+            }
+        }
+    }
+}
